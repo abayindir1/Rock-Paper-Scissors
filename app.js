26f0b72f55038da8f8cfec1ad707fb84b2ea2587
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-08-25 21:48:13 -0500

    modal structure and functionality

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var uScore = 0;
-  var cScore = 0;
-
   const options = document.querySelectorAll(".option-img");
+  const result = document.getElementById("result");
+  const modal = document.getElementsByClassName("modal")[0];
+
+  var userScore = 0;
+  var computerScore = 0;
+
   const computerOptions = ["rock", "paper", "scissors"];
 
   options.forEach((option) => {
@@ -15,12 +18,39 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log(cPick);
 
       //   Compare picks
-      if(uPick === cPick){
-        console.log("bum")
-      } else if((cPick === "scissors" && uPick === "paper") || (cPick === "rock" && uPick === "scissors") || (cPick === "paper" && uPick === "rock")){
-        console.log("you lost")
-      }else{
-        console.log("you won")
+      if (uPick === cPick) {
+        // if tie
+        modal.style.display = "initial";
+        result.innerHTML = `
+        <h1 class="text-win">It's a Draw!</h1>
+          
+          <p>Computer Chose <strong>${cPick}</strong></p>
+          <p>You Chose <strong>${uPick}</strong></p>`
+        console.log("bum");
+      } else if (
+        (cPick === "scissors" && uPick === "paper") ||
+        (cPick === "rock" && uPick === "scissors") ||
+        (cPick === "paper" && uPick === "rock")
+        ) {
+          // if computer wins
+        computerScore++;
+        modal.style.display = "initial";
+        result.innerHTML = `
+        <h1 class="text-win">You Lost!</h1>
+          
+        <p>Computer Chose <strong>${cPick}</strong></p>
+        <p>You Chose <strong>${uPick}</strong></p>`
+        console.log("you lost");
+      } else {
+        // if user wins
+        userScore++;
+        modal.style.display = "initial";
+        result.innerHTML = `
+        <h1 class="text-win">You Won!</h1>
+          
+        <p>Computer Chose <strong>${cPick}</strong></p>
+        <p>You Chose <strong>${uPick}</strong></p>`
+        console.log("you won");
       }
     });
     // console.log(option)
